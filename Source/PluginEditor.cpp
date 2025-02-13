7ae1ef9fefe401e94341b943980f5d653205496a
kaasch-co <toby@kaasch.dk>
2025-02-14 00:54:27 +0100

    Added template volume slider to the editor.

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -7,6 +7,7 @@
 */
 
 #include "PluginProcessor.h"
+#include "juce_gui_basics/juce_gui_basics.h"
 #include "PluginEditor.h"
 
 //==============================================================================
@@ -16,6 +17,15 @@ VstpluginbaseAudioProcessorEditor::VstpluginbaseAudioProcessorEditor (Vstpluginb
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
+
+	volumeSlider.setSliderStyle(juce::Slider::LinearBarVertical);
+	volumeSlider.setRange(0u, 127u, 1u);
+	volumeSlider.setTextBoxStyle(juce::Slider::NoTextBox, false, 90, 0);
+	volumeSlider.setPopupDisplayEnabled(true, false, this);
+	volumeSlider.setTextValueSuffix("Volume");
+	volumeSlider.setValue(0u);
+
+	addAndMakeVisible(&volumeSlider);
 }
 
 VstpluginbaseAudioProcessorEditor::~VstpluginbaseAudioProcessorEditor()
@@ -26,15 +36,16 @@ VstpluginbaseAudioProcessorEditor::~VstpluginbaseAudioProcessorEditor()
 void VstpluginbaseAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll(getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.setColour(juce::Colours::white);
+    g.setFont(juce::FontOptions (15.0f));
+    g.drawFittedText("Input test", getLocalBounds(), juce::Justification::topLeft, 1);
 }
 
 void VstpluginbaseAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+	
+	volumeSlider.setBounds(40, 30, 20, getHeight() - 60);
 }
