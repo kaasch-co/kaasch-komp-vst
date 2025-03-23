4567b4a0361e8ee5f5b221682f93cb73eb7f11af
kaasch-co <toby@kaasch.dk>
2025-03-23 02:09:45 +0100

    better justification for text as well

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -7,6 +7,7 @@
 */
 
 #include "PluginProcessor.h"
+#include "juce_graphics/juce_graphics.h"
 #include "juce_gui_basics/juce_gui_basics.h"
 #include "PluginEditor.h"
 #include "EditorUtils.h"
@@ -41,13 +42,25 @@ VstpluginbaseAudioProcessorEditor::~VstpluginbaseAudioProcessorEditor()
 }
 
 //==============================================================================
-void VstpluginbaseAudioProcessorEditor::paint(juce::Graphics& g)
-{
+void VstpluginbaseAudioProcessorEditor::paint(juce::Graphics& g) {
+
+	const int W = getWidth();
+	const int H = getHeight();
+	const int PAD = 30;
+
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
     g.setColour(juce::Colours::white);
     g.setFont(12.0f);
-    g.drawFittedText("Kaasch compressor", getLocalBounds(), juce::Justification::centredTop, 1);
+    g.drawFittedText(
+		"KAASCH Kompressor",
+		0,
+		0,
+		W,
+		H,
+		juce::Justification::centredTop,
+		1
+	);
 }
 
 void VstpluginbaseAudioProcessorEditor::resized()
@@ -55,16 +68,16 @@ void VstpluginbaseAudioProcessorEditor::resized()
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
 	
-	const int H = getHeight();
 	const int W = getWidth();
+	const int H = getHeight();
 	const int W_SLIDER = 40;
 	const int PAD = 30;
 
 	// bitshift by 1 as quick 2x multiplier
-	inputGainSlider.setBounds(PAD, PAD, W_SLIDER, H - (PAD << 1));
-	thresholdSlider.setBounds(PAD + 2 * (W_SLIDER + PAD), PAD, W_SLIDER, H - (PAD << 1));
-	outputGainSlider.setBounds(W - W_SLIDER - PAD, PAD, W_SLIDER, H - (PAD << 1));
-	attackTimeSlider.setBounds(PAD * 2 + W_SLIDER, PAD, W_SLIDER, H - (PAD << 1));
-	releaseTimeSlider.setBounds(W - ((W_SLIDER + PAD) << 1), PAD, W_SLIDER, H - (PAD << 1));
+	inputGainSlider.setBounds(PAD, PAD << 1, W_SLIDER, H - (PAD << 1));
+	thresholdSlider.setBounds(PAD + 2 * (W_SLIDER + PAD), PAD << 1, W_SLIDER, H - (PAD << 1));
+	outputGainSlider.setBounds(W - W_SLIDER - PAD, PAD << 1, W_SLIDER, H - (PAD << 1));
+	attackTimeSlider.setBounds(PAD * 2 + W_SLIDER, PAD << 1, W_SLIDER, H - (PAD << 1));
+	releaseTimeSlider.setBounds(W - ((W_SLIDER + PAD) << 1), PAD << 1, W_SLIDER, H - (PAD << 1));
 
 }
